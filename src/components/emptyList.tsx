17ca6cfcf53ec09fe8a42ff224b770d618cdc368
Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-05-09 10:17:09 -0300

    feat(components): add emptyList component

---
+++ src/components/emptyList.tsx
@@ -0,0 +1,13 @@
+export function EmptyList() {
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-10">
+      <h2 className="text-h2 text-primary">Clique em Começar para jogar</h2>
+
+      <img
+        src="img/stickers/bruce-03.png"
+        alt="Logo"
+        className="w-40 object-contain select-none"
+      />
+    </div>
+  );
+}
