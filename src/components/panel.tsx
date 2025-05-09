7453931163e3d1c3b3c2fe950db5e90d8a8b546b
Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-05-09 10:17:33 -0300

    feat(Panel): adjust style

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -5,12 +5,12 @@ type PanelProps = {
 
 export function Panel({ title, value }: PanelProps) {
   return (
-    <div className="flex flex-col items-center justify-center w-full">
-      <h2 className="text-body font-bold text-primary text-nowrap px-2">
+    <div className="flex flex-col items-center justify-center w-full hover:cursor-default select-none">
+      <h2 className="text-body font-bold text-slate-500 text-nowrap px-2">
         {title}
       </h2>
-      <div className="w-full min-w-40 flex flex-col items-center justify-center p-4 rounded-xl bg-white shadow-md">
-        <span className="text-body-highlight">{value}</span>
+      <div className="w-full min-w-40 flex flex-col items-center justify-center p-4 rounded-xl bg-orange-100 shadow-md">
+        <span className="text-body-highlight text-primary">{value}</span>
       </div>
     </div>
   );
